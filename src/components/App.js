ac495db457fc2fb8dbd7a773ec005cf053a7e62e
Emmanuel Alves <emmanuelalves@locus.software>
2019-09-20 18:04:00 -0300

    Changed static data to prop data

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,24 @@ import React from 'react';
 
 import PlacarContainer from './PlacarContainer';
 
+const dados = {
+    partida: {
+        estadio: "Maracanã/RJ",
+        data: "20/09/2019",
+        horario: "19:00",
+    },
+    casa: {
+        nome: "Vasco"
+    },
+    visitante: {
+        nome: "Flamengo"
+    },
+};
+
 export default class App extends React.Component {
     render() {
         return (
-            <PlacarContainer />
+            <PlacarContainer partida={dados.partida} casa={dados.casa} visitante={dados.visitante} />
         );
     }
 }
